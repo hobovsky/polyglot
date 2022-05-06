1b2aaf532d7f70eeb05cd8fd355444c714739683
hobovsky <hobson@wp.pl>
2022-05-06 15:47:13 +0200

    Bump up script version

--- polyglot.js
+++ polyglot.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name    CodeWars - Mark solved languages
-// @version 1.3.14
+// @version 1.3.15
 // @downloadURL https://github.com/hobovsky/polyglot/releases/latest/download/polyglot.js
 // @include https://www.codewars.com/*
 // @grant   GM_xmlhttpRequest
