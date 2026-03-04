ad82d904e4a1f4a3addd2bc0aea4a7e24bffb1b2
hobovsky <hobson@wp.pl>
2026-03-04 23:16:08 +0100

    Remove redundant readme preamble

--- polyglot.js
+++ polyglot.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name    Polyglot for Codewars
-// @description User script which provides some extra functionalities to Codewars
+// @description Small, opt-in quality-of-life improvements for Codewars.
 // @version 1.19.1
+// @homepageURL https://github.com/hobovsky/polyglot
+// @supportURL  https://github.com/hobovsky/polyglot/issues
 // @downloadURL https://github.com/hobovsky/polyglot/releases/latest/download/polyglot.js
-// @updateURL https://github.com/hobovsky/polyglot/releases/latest/download/polyglot.js
+// @updateURL   https://github.com/hobovsky/polyglot/releases/latest/download/polyglot.js
 // @match https://www.codewars.com/*
 // @grant   GM_xmlhttpRequest
 // @grant   GM_setValue
@@ -20,74 +22,14 @@
 // @require https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
 // ==/UserScript==
 
-/****************************************************************
+/*
+Polyglot is a Tampermonkey userscript for Codewars with small, opt-in UI improvements.
 
-WHAT IS IT?
------------
- This piece of code is a Tampermonkey user script which provides some
- additional per-language filtering and display capabilities,
- effectively making it easier for you to obtain POLYGLOT badge.
+Project home / docs: https://github.com/hobovsky/polyglot
+Download (latest):   https://github.com/hobovsky/polyglot/releases/latest/download/polyglot.js
+Bugs / ideas:        https://github.com/hobovsky/polyglot/issues
+*/
 
-WHERE CAN I DOWNLOAD IT FROM?
------------------------------
- You can get the script [here](https://github.com/hobovsky/polyglot/releases/latest/download/polyglot.js).
-
- HOW TO INSTALL IT?
-------------------
- - Install Tampermonkey extension for your browser,
- - Copy&paste the script to your scripts library.
-
- WHAT FEATURES DOES IT PROVIDE?
-------------------------------
- - You can copy content of code boxes to clipboard.
- - "Spoiler" flags are visible all the time and do not dis/re-appear in a very annoying manner.
- - Contents of "Solutions" and "Past solutions" views are displayed in tabs by language.
- - Leaderboards: "Solved kata is default leaderboard (since "Overall"
-   ranking does not measure anything useful). Also, leaderboards are
-   automatically scrolled to show your score.
- - Beta kata: uses Codwewars API to fetch and present breakdown of rank votes.
- - Show attempted languages of a user in "Discourse".
- - Show timestamps of solution groups.
- - Show a toggle for raw markdown comments.
- - Show notifications after ranking up on leaderboards.
-
- HOW TO UNINSTALL IT?
---------------------
- I haven't checked.
-
- KNOWN ISSUES
-------------
- - Yes.
- - A race condition here or there.
- - Selectors, hooks and listeners used are so inefficient that your local power
-   plant probably doubles its coal consumption.
-
- WHAT CAN I DO WITH THE SCRIPT?
-------------------------------
- - You are allowed to use it, unless someone authoritative (CW staff?) says you can't.
- - You can modify it ONLY IF your modifications are going to bring any improvement
-   into the way it works, AND you are going to share improved version with CW community.
- - You can send all your critical remarks to `/dev/null`, unless it's something I could
-   learn or otherwise benefit from - in such case, you can contact me on Codewars
-   Discord server.
-
- THIS CODE IS CRAP, LOOKS LIKE CRAP, AND WORKS LIKE CRAP! WHY?
--------------------------------------------------------------
- I am really sorry if this code hurts your eyes, brain, or feelings
- in any way, but I am not a professional HTML developer and each and
- every technique present here (JavaScript, jQuery, TamperMonkey,
- CW API) I've used for the first time.
-
- CREDITS
--------
- - Codewars
- - TamperMonkey
- - StackOverflow
- - jQuery
- - notify.js
- - Chart.js
-
-****************************************************************/
 /* eslint no-multi-spaces: off */
 /* globals jQuery, $, waitForKeyElements, App */
 var $ = window.jQuery;
